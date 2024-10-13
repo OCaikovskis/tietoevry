08bc03b98ed287030be1247b4a1dcbbba2aacad1
OCaikovskis <o.caikovskis@gmail.com>
2024-10-13 22:53:29 +0300

    Changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Tietoevry home work",
+  title: "Tietoevry homework",
   description: "Created by Oskars Caikovskis",
 };
 
